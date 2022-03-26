cd310e12959d04256a04a90e1208771ac332856e
Yafim Vainilovich <yafimsva@gmail.com>
2022-03-26 17:13:43 -0400

    changing vite config fule

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/catan-dice",
+  base: "/catan-dice/",
   plugins: [vue()],
   resolve: {
     alias: {
